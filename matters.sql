769587d6c9a20da96c1910baaa7125dd9242fc98
Sreevishnu <sreeab28@gmail.com>
2020-10-25 14:24:14 +0530

    adding sql script

---
+++ matters.sql
@@ -0,0 +1,61 @@
+CREATE TYPE tstatus AS ENUM('Not started', 'In progress', 'Completed');
+CREATE TYPE ttype AS ENUM('Short-term', 'Long-term');
+
+CREATE TABLE IF NOT EXISTS users(
+  email VARCHAR PRIMARY KEY,
+  username VARCHAR,
+  password VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id UUID PRIMARY KEY,
+  title VARCHAR,
+  task_type ttype,
+  task_status tstatus DEFAULT 'Not started',
+  owner_id VARCHAR REFERENCES users(email) ON DELETE CASCADE,
+  date_created TIMESTAMPTZ,
+  date_updated TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS task_items(
+  item_id UUID PRIMARY KEY,
+  task_id UUID REFERENCES tasks(id) ON DELETE CASCADE,
+  item_title VARCHAR,
+  details VARCHAR,
+  item_status tstatus DEFAULT 'Not started',
+  date_created TIMESTAMPTZ,
+  date_updated TIMESTAMPTZ
+);
+
+CREATE OR REPLACE FUNCTION update_status()
+RETURNS TRIGGER
+AS $$
+DECLARE
+  count_inprog integer := 0;
+  count_completed integer := 0;
+  count_notstarted integer := 0;
+  new_status tstatus := 'Not started'; 
+BEGIN
+  SELECT count(*) INTO count_notstarted FROM task_items
+    WHERE task_id = NEW.task_id AND item_status = 'Not started';
+  SELECT count(*) INTO count_inprog FROM task_items
+    WHERE task_id = NEW.task_id AND item_status = 'In progress';
+  SELECT count(*) INTO count_completed FROM task_items
+    WHERE task_id = NEW.task_id AND item_status = 'Completed';
+  
+  IF (count_completed <> 0 AND count_inprog = 0 AND count_notstarted = 0) THEN
+    new_status = 'Completed';
+  ELSIF (count_inprog <> 0 OR (count_notstarted <> 0 AND count_completed <> 0)) THEN
+    new_status = 'In progress';
+  END IF;
+
+  UPDATE tasks SET task_status = new_status, date_updated = NEW.date_updated WHERE id = NEW.task_id;
+  RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_task_status
+  AFTER DELETE OR UPDATE OF item_status
+  ON task_items
+  EXECUTE FUNCTION update_status();
